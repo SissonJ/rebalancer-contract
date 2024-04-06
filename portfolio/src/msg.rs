6b36edb47f7f8b1a0f70cd72b33cf67c1a50f125
SissonJ <sissonjack67@gmail.com>
2024-04-06 10:21:03 -0500

    add back portfolio

---
+++ portfolio/src/msg.rs
@@ -0,0 +1,72 @@
+use crate::state::{ContractStatus, PortfolioConfig};
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Binary, ContractInfo, Uint128, Uint256};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub admin: Addr,
+    pub viewing_key: String,
+    pub swap_factory: ContractInfo,
+    pub withdraw_fee: Uint128,
+    pub create_fee: Uint128,
+    pub snip20_code_id: i32,
+    pub portfolio_code_id: i32,
+    pub accepted_deposit_tokens: Option<Vec<ContractInfo>>,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    // ADMIN
+    UpdateConfig {
+        admin: Option<Addr>,
+        swap_factory: Option<ContractInfo>,
+        withdraw_fee: Option<i32>,
+        create_fee: Option<i32>,
+        snip20_code_id: Option<i32>,
+        accepted_deposit_tokens: Option<Vec<ContractInfo>>,
+        contract_status: Option<ContractStatus>,
+    },
+    RegisterAssets {
+        assets: Vec<ContractInfo>,
+    },
+    // Responsible for rebalancing protfolios
+    // Will reset UNUPDATED_LIST if found empty
+    Update {
+        batch_amount: Option<Uint128>,
+    },
+
+    //Receiver interface
+    Receive {
+        sender: Addr,
+        from: Addr,
+        amount: Uint256,
+        #[serde(default)]
+        msg: Option<Binary>,
+    },
+}
+
+#[cw_serde]
+pub enum ReceiveMsg {
+    CreatePortfolio {
+        config: Vec<PortfolioConfig>,
+        name: String,
+    },
+    Withdraw {},
+    Deposit {
+        portfolio_snip20: Addr,
+    },
+}
+
+#[cw_serde]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetConfig {},
+    GetState {},
+    GetUnupdated {},
+}
+
+#[cw_serde]
+pub enum QueryResponse {}
+
+#[cw_serde]
+pub enum ExecuteResponse {}
---
+++ portfolio/src/state.rs
@@ -0,0 +1,66 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, ContractInfo, Storage, Uint128};
+use secret_toolkit::storage::Item;
+use secret_toolkit::{serialization::Json, storage::Keymap};
+
+pub static CONFIG_KEY: &[u8] = b"config";
+
+#[cw_serde]
+pub enum ContractStatus {
+    ACTIVE,    // full funcitonality
+    FROZEN,    // no functionality
+    PROTECTED, // withdraw functionality only
+}
+
+#[cw_serde]
+pub struct Config {
+    pub admin: Addr,
+    pub swap_factory: ContractInfo,
+    pub withdraw_fee: Uint128,
+    pub create_fee: Uint128,
+    pub snip20_code_id: i32,
+    pub portfolio_code_id: i32,
+    pub accepted_deposit_tokens: Vec<ContractInfo>,
+    pub contract_status: ContractStatus,
+}
+
+#[cw_serde]
+pub struct PortfolioConfig {
+    pub percent: u32,
+    pub asset: ContractInfo,
+}
+
+#[cw_serde]
+pub struct Portfolio {
+    pub config: Vec<PortfolioConfig>,
+    pub creator: Addr,
+    pub name: String,
+    pub snip20: ContractInfo,
+}
+
+#[cw_serde]
+pub struct RouteKey(Addr, Addr);
+
+#[cw_serde]
+pub struct SwapContract {
+    addr: Addr,
+    code_hash: String,
+}
+
+pub const KEY_CONFIG: &[u8] = b"config";
+pub const KEY_PORTFOLIO_LIST: &[u8] = b"portfolio_list";
+pub const KEY_UNUPDATED_LIST: &[u8] = b"unupdated_list";
+pub const KEY_REGISTERED_ASSETS: &[u8] = b"registered_assets";
+pub const KEY_VIEWING_KEY: &[u8] = b"viewing_key";
+pub const KEY_PORTFOLIO: &[u8] = b"portfolio";
+pub const KEY_ROUTE_CACHE: &[u8] = b"route_cache";
+pub static CONFIG: Item<Config> = Item::new(KEY_CONFIG);
+// List of all known portfolios
+pub static PORTFOLIO_LIST: Item<Vec<Addr>> = Item::new(KEY_PORTFOLIO_LIST);
+// List of all portfolios pending update
+pub static UNUPDATED_LIST: Item<Vec<Addr>> = Item::new(KEY_UNUPDATED_LIST);
+pub static REGISTERED_ASSETS: Item<Vec<Addr>> = Item::new(KEY_REGISTERED_ASSETS);
+pub static VIEWING_KEY: Item<String> = Item::new(KEY_VIEWING_KEY);
+// Hash map of snip20 protfolio token and the portfolio information
+pub static PORTFOLIO: Keymap<Addr, Portfolio> = Keymap::new(KEY_PORTFOLIO);
+pub static ROUTE_CACHE: Keymap<RouteKey, Vec<SwapContract>> = Keymap::new(KEY_ROUTE_CACHE);
